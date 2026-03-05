574b9ac9734874fd735f3ba0fb07b69882bead9b
PiyushSingh-Chuphal <piyushsingh00812621@gmail.com>
2026-03-06 00:48:01 +0530

    Create Two_Sum.cpp
    Added Two Sum solution

---
+++ ARRAY/Two_Sum.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> arr(2);
+        for(int i = 0 ; i < nums.size() ; i++){
+            for(int j = i + 1 ; j < nums.size() ; j++){
+                if(nums[i] + nums[j] == target){
+                    arr[0] = i;
+                    arr[1] = j;
+                    return arr;
+                }
+            }
+        }
+        return arr;
+    }
+};
+
+int main() {
+    Solution sol;
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+
+    vector<int> result = sol.twoSum(nums, target);
+
+    cout << "[" << result[0] << "," << result[1] << "]" << endl;
+
+    return 0;
+}
