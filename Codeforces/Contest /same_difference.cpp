3381da52718ac9aefa168d71ca4c6d9af6a773bb
PiyushSingh-Chuphal <piyushsingh00812621@gmail.com>
2026-03-07 22:32:27 +0530

    Create same_difference.cpp

---
+++ Codeforces/Contest /same_difference.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        string s;
+        cin >> s;
+ 
+        char last = s[n - 1];
+        int operations = 0;
+ 
+        for (int i = 0; i < n; i++) {
+            if (s[i] != last) {
+                operations++;
+            }
+        }
+ 
+        cout << operations << "\n";
+    }
+ 
+    return 0;
+}
