e43ff0d6ca5dfd8c2a4e782277a76eb267064a46
PiyushSingh-Chuphal <piyushsingh00812621@gmail.com>
2026-03-18 20:58:34 +0530

    Update ADD_two_numbers.cpp

--- Leet_code/ARRAY/ADD_two_numbers.cpp
+++ Leet_code/ARRAY/ADD_two_numbers.cpp
@@ -1,6 +1,7 @@
 
 class Solution {
 public:
+// this is going to be the linked list questionb
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
         ListNode* l3 = new ListNode(0);
         ListNode *Head = l3;
