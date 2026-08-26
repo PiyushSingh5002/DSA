07a2612450f4a22d18b742dd0e96c74276ce67ba
PiyushSingh-Chuphal <178661553+PiyushSingh5002@users.noreply.github.com>
2026-08-27 00:08:13 +0530

    Update 2250A.cpp

--- Codeforces/800-1000/2250A.cpp
+++ Codeforces/800-1000/2250A.cpp
@@ -7,18 +7,15 @@ int main() {
         int n; cin >> n;
         vector<long long> w(n);
         for (auto &x : w) cin >> x;
-        
-        if (n % 2 != 0) {
-            cout << "NO\n";
-            continue;
+        if(n % 2 != 0){
+            cout<<NO<<endl;
         }
-        
-        long long minOdd = LLONG_MAX, maxEven = LLONG_MIN;
+        long long maxEven = LLONG_MIN, minOdd = LLONG_MAX;
         for (int i = 0; i < n; i++) {
-            if (i % 2 == 0) minOdd = min(minOdd, w[i]);   
-            else maxEven = max(maxEven, w[i]);            
-        }
-        
-        cout << ((minOdd - maxEven >= 2) ? "YES" : "NO") << "\n";
+            if(i % 2 == 0)minOdd = min(w(i)  , minOdd);
+            else maxEven = max(w(i)  , maxEven);
+        } 
+
+        cout << ((minOdd - maxEven) >=2 ? "YES" : "NO") << "\n";
     }
 }
