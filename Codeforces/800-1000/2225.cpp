af1a2b2b215ef234f108a30d5eda46960cb06582
PiyushSingh-Chuphal <178661553+PiyushSingh5002@users.noreply.github.com>
2026-08-31 20:36:44 +0530

    Create 2225.cpp

---
+++ Codeforces/800-1000/2225.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        long long x, y;
+        cin >> x >> y;
+        long long  k = y/x;
+        cout<<(k >= 3 ? "YES":"NO")<<endl;
+    }
+}
