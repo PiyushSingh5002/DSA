fdd5f2467430d760a7b72b38553bfc5086a9ed93
PiyushSingh-Chuphal <178661553+PiyushSingh5002@users.noreply.github.com>
2026-09-08 17:09:10 +0530

    Create 2237A.cpp

---
+++ Codeforces/800-1000/2237A.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+
+        vector<int> a(n);
+
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        int min_val = INT_MAX;
+        int ans = 0;
+        for(int i = 0 ; i < n ; i++){
+            if(a[i] < min_val){
+                min_val = a[i];
+            }else{
+                a[i] -= min_val;
+            }
+            ans += a[i];
+        }
+        cout << ans << '\n';
+    }
+
+    return 0;
+}
