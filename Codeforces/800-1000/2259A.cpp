83412e96e96116e77f22884e7cf085d4e7652fce
PiyushSingh-Chuphal <178661553+PiyushSingh5002@users.noreply.github.com>
2026-09-17 23:17:45 +0530

    Create 2259A.cpp

---
+++ Codeforces/800-1000/2259A.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    scanf("%d", &t);
+    while (t--) {
+        int n, k;
+        scanf("%d %d", &n, &k);
+        char s[25];
+        scanf("%s", s);
+        int ans = 0;
+        for (int i = 0; i < n; i += k) {
+            bool hasZero = false;
+            for (int j = i; j < i + k; j++) {
+                if (s[j] == '0') { hasZero = true; break; }
+            }
+            if (!hasZero) ans++;
+        }
+        printf("%d\n", ans);
+    }
+    return 0;
+}
